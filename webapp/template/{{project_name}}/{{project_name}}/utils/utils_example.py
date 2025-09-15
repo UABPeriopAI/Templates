194ef2c02f3e3d70eceba78470ca79e589da70ad
Ryan Godwin <ryangodwin@uabmc.edu>
2025-09-15 10:00:49 -0400

    Moving UI to top level to streamline imports and moving utils_example to utils.

--- webapp/template/{{project_name}}/{{project_name}}/utils/utils_example.py
+++ webapp/template/{{project_name}}/{{project_name}}/utils/utils_example.py
@@ -1,5 +1,21 @@
 import pandas as pd
-#Add any project-specific utility functions here.
-def placeholder():
-    """This is a place holder function"""
-    pass
+
+# Add any project-specific utility functions here.
+
+
+def task_a():
+    """
+    This is the documentation for example task a
+    """
+    text_out = "Demonstrating Streamlit behavior: You ran task a"
+    print(text_out)
+    return text_out
+
+
+def task_b():
+    """
+    This is the documentation for example task b
+    """
+    text_out = "Demonstrating Streamlit behavior: You ran task b"
+    print(text_out)
+    return text_out
